42e18b261af8c248b65d842c2be51343457a4df1
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-09-18 15:44:10 +0300

    Add classes supprting multi-threading

---
+++ src/ConcurrentNode.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+
+namespace SkipLists {
+
+    internal sealed class ConcurrentNode<K,V> : Node<K,V> {
+
+        private readonly ReaderWriterLockSlim nextLock = new ReaderWriterLockSlim();
+        private readonly ReaderWriterLockSlim belowLock = new ReaderWriterLockSlim();
+
+        internal override Node<K, V> Next {
+            get {
+                try {
+                    nextLock.EnterReadLock();
+                    return next;
+                }
+                finally {
+                    nextLock.ExitReadLock();
+                }
+            }
+
+            set {
+                try {
+                    nextLock.EnterWriteLock();
+                    next = value;
+                }
+                finally {
+                    nextLock.ExitWriteLock();
+                }
+            }
+        }
+
+        internal override Node<K, V> Below {
+            get {
+                try {
+                    belowLock.EnterReadLock();
+                    return next;
+                }
+                finally {
+                    belowLock.ExitReadLock();
+                }
+            }
+
+            set {
+                try {
+                    belowLock.EnterWriteLock();
+                    next = value;
+                }
+                finally {
+                    belowLock.ExitWriteLock();
+                }
+            }
+        }
+
+        public ConcurrentNode() : base() {}
+
+        public ConcurrentNode(K key, V value): base(key, value) {}
+    }
+}
---
+++ src/ConcurrentSkipList.cs
@@ -0,0 +1,18 @@
+﻿namespace SkipLists {
+
+    internal class ConcurrentSkipList<K,V> : SkipList<K, V> where V : class {
+
+        public ConcurrentSkipList(SkipList<K,V> otherList) : base(otherList.Comparer) {
+            foreach (var pair in otherList.GetEntries())
+                Insert(pair.Key, pair.Value);
+        }
+
+        private protected override  Node<K, V> BuildNode(K key, V value) {
+            return new ConcurrentNode<K, V>(key, value);
+        }
+        private protected override Node<K, V> BuildNode() {
+            return new ConcurrentNode<K, V>();
+        }
+    }
+
+}
