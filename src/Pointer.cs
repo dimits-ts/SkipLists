b9938929c3ce3aab3e471d40e0f86c0ce9627f50
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-09-20 17:22:57 +0300

    Add reference wrapper class

---
+++ src/Pointer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SkipLists {
+    /// <summary>
+    /// A reference wrapper pointing to an object or struct, in order to 
+    /// make the return value nullable in every case.
+    /// </summary>
+    /// <typeparam name="T">The type of the object boxed in the wrapper.</typeparam>
+    internal sealed class Pointer<T> {
+        private readonly T value;
+        private readonly bool isNull;
+        
+        public T Value {
+            get {
+                if (isNull)
+                    throw new ArgumentNullException("There is no value stored in the pointer");
+                return value;
+            }
+        }
+
+        public bool IsNull {
+            get {
+                return isNull;
+            }
+        }
+
+        public Pointer(T value) {
+            this.value = value;
+            isNull = false;
+        }
+
+        public Pointer() {
+            this.value = default(T);
+            isNull = true;
+        }
+    }
+}
