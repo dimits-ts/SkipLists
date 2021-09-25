c2e3242d955a4be7a7dcf4b706c97756d285c8fc
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-09-25 18:33:21 +0300

    Add demo source and data files

---
+++ Demo/Demo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using SkipLists;
+
+namespace FlightDemo {
+
+    public static class Demo {
+        
+        public static void Main() {
+
+            //get file name
+            Console.Write("Enter the path to the flights file:\n>");
+            string file = Console.ReadLine();
+            if (!File.Exists(file)) {
+                Console.WriteLine("Invalid path: " + file);
+                return;
+            }
+
+            //get departure and arrival airports
+            Console.Write("Enter the closest airport to you:\n>");
+            string startName = Console.ReadLine().ToUpper();
+            Console.Write("Enter the airport closest to your destination:\n>");
+            string destinationName = Console.ReadLine().ToUpper();
+
+            //get departure and arrival times
+            DateTime startTime;
+            DateTime arrivalTime;
+            try {
+                Console.Write("Enter the ideal time of your flight's departure in HH::MM format:\n>");
+                startTime = DateTime.ParseExact(Console.ReadLine(), "HH:mm", null);
+                Console.Write("Enter the ideal time of your flight's arrival in HH::MM format:\n>");
+                arrivalTime = DateTime.ParseExact(Console.ReadLine(), "HH:mm", null);
+            } catch(FormatException exc) {
+                Console.WriteLine("Invalid time: " + exc.Message);
+                return;
+            }
+
+            //calculate best flights
+            var startTimeDict = new SkipListDictionary<TimeSpan, FlightInfo>();
+            var arrivalTimeDict =  new SkipListDictionary<TimeSpan, FlightInfo>();
+
+            IEnumerable<FlightInfo> flights;
+            try {
+                flights = ReadFile(file);
+            } catch(IOException exc) {
+                Console.WriteLine("Could not access flights file: " + exc.Message);
+                return;
+            }
+            
+            foreach (FlightInfo flight in flights) {
+                if (flight.Start == startName && flight.Destination == destinationName) {
+                    startTimeDict.Add((flight.StartTime - startTime).Duration(), flight);
+                    arrivalTimeDict.Add((flight.ArrivalTime - arrivalTime).Duration(), flight);
+                }
+            }
+            
+            //print best flights
+            if(startTimeDict.Count == 0) {
+                Console.WriteLine(string.Format("There are no scheduled flights between {0} and {1}.",
+                    startName, destinationName));
+                return;
+            }
+
+            Console.WriteLine("Most convinient flights to leave:\n" + 
+                PrintFlights(startTimeDict.Values, 5));
+
+            Console.WriteLine("Most convinient flights to arrive:\n" +
+                PrintFlights(arrivalTimeDict.Values, 5));
+        }
+
+        private static IEnumerable<FlightInfo> ReadFile(string filePath) {
+            LinkedList<FlightInfo> flightList = new LinkedList<FlightInfo>();
+
+            foreach (string flight in File.ReadAllLines(filePath))
+                flightList.AddLast(ParseFlightData(flight));
+
+            return flightList;
+        }
+
+        private static FlightInfo ParseFlightData(string data) {
+            string[] tokens = data.Split(" ");
+            return new FlightInfo(tokens[0], tokens[1], tokens[2], tokens[3]);
+        }
+
+        private static string PrintFlights(IEnumerable<FlightInfo> flights, int count) {
+            StringBuilder str = new StringBuilder();
+            int flightsPrinted = 0;
+
+            foreach (var flight in flights) {
+                str.AppendLine(flight.ToString());
+                flightsPrinted++;
+                if (flightsPrinted >= count)
+                    break;
+            }
+
+            return str.ToString();
+        }
+    }
+}
---
+++ Demo/FlightInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FlightDemo {
+
+    /// <summary>
+    /// A struct containing information about a flight on a given day.
+    /// </summary>
+    public readonly struct FlightInfo {
+        private readonly string start;
+        private readonly string destination;
+        private readonly DateTime start_time;
+        private readonly DateTime arrival_time;
+
+        public FlightInfo(string start, string destination, string start_time, string arrival_time) {
+            this.start = start;
+            this.destination = destination;
+            this.start_time = DateTime.ParseExact(start_time, "HH:mm", null);
+            this.arrival_time = DateTime.ParseExact(arrival_time, "HH:mm", null);
+        }
+
+        public string Start {
+            get {
+                return start;
+            }
+        }
+
+        public string Destination {
+            get {
+                return destination;
+            }
+        }
+
+        public DateTime StartTime {
+            get {
+                return start_time;
+            }
+        }
+
+        public DateTime ArrivalTime {
+            get {
+                return arrival_time;
+            }
+        }
+
+        public override string ToString() {
+            return string.Format("{0} to {1} leaving at {2}:{3} and arriving at {4}:{5}",
+                start, destination, HourFormat(start_time.Hour), HourFormat(start_time.Minute),
+                HourFormat(arrival_time.Hour), HourFormat(arrival_time.Minute));
+        }
+
+        private static string HourFormat(int time) {
+            string str = time.ToString();
+
+            if (str.Length == 1)
+                return "0" + str;
+            else
+                return str;
+        }
+    }
+}
